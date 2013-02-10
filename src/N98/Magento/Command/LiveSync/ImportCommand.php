07c1970eed0419d3bc59169b19357ce0d3cbc09e
Kalen Jordan <kalen@sparcksoft.com>
2013-02-09 23:17:06 -0800

    Ability to specify a Magento path to the import source Ability to filter out specific packets that you want to import

--- src/N98/Magento/Command/LiveSync/ImportCommand.php
+++ src/N98/Magento/Command/LiveSync/ImportCommand.php
@@ -13,10 +13,15 @@ use Symfony\Component\Console\Output\NullOutput;
 
 class ImportCommand extends AbstractMagentoCommand
 {
+    /** @var InputInterface */
+    protected $_input;
+
     protected function configure()
     {
         $this
             ->setName('livesync:import')
+            ->addOption('filter', null, InputOption::VALUE_OPTIONAL, "A filter to only process packets that match")
+            ->addArgument('source', InputArgument::REQUIRED, "The source magento instance to import from")
             ->setDescription('Imports packets from a Magento instance using KJ_LiveSync module')
         ;
     }
@@ -35,10 +40,47 @@ class ImportCommand extends AbstractMagentoCommand
 
         $output->writeln("<info>Running LiveSync import</info>");
 
-        $import = $this->getImportModel();
-        $import->run();
+        $import = $this->getImportModel()
+            ->setMagentoSource($this->_getSourceArgument());
+        $files = $import->getFiles();
+        foreach ($files as $file) {
+            if ($this->_isFileFilteredOut($file)) {
+                continue;
+            }
+
+            $output->writeln("<info>Processing $file");
+            $import->processPacket($file);
+        }
     }
 
+    protected function _isFileFilteredOut($file)
+    {
+        if (! $this->_input->getOption('filter')) {
+            return false;
+        }
+
+        $filter = $this->_input->getOption('filter');
+        $filter = str_replace("/", "\/", $filter);
+        if (preg_match('/' . $filter . '/', $file, $match)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function _getSourceArgument()
+    {
+        $source = $this->_input->getArgument('source');
+        if (substr($source, -1) == '/') {
+            $source = substr($source, 0, strlen($source) - 1);
+        }
+
+        return $source;
+    }
+
+    /**
+     * @return \KJ_LiveSync_Model_Import
+     */
     protected function getImportModel()
     {
         if (!class_exists('KJ_LiveSync_Model_Import')) {
